12e5145274ffa86efeda6d3dfde23439c4237aeb
rifqi2242 <rifqimuzakki45@gmail.com>
2024-10-15 13:34:52 +0700

    create routing system

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import StartScreen from '../screens/StartScreen';
+import Main from '../screens/TabScreen/Main';
+import Detail from '../screens/Detail';
+import Order from '../screens/Order';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="StartScreen">
+        <Stack.Screen name="StartScreen" component={StartScreen} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Detail" component={Detail} />
+        <Stack.Screen name="Order" component={Order} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image, Text} from 'react-native';
+import Main from '../screens/TabScreen/Main';
+import Love from '../screens/TabScreen/Love';
+import Basket from '../screens/TabScreen/Basket';
+import Notification from '../screens/TabScreen/Notification';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}: any) => ({
+        tabBarIcon: ({focused}: any) => {
+          let iconSource;
+
+          if (route.name === 'Main') {
+            iconSource = focused
+              ? require('../assets/icons/main-active.png')
+              : require('../assets/icons/main.png');
+          } else if (route.name === 'Love') {
+            iconSource = focused
+              ? require('../assets/icons/love-active.png')
+              : require('../assets/icons/love.png');
+          } else if (route.name === 'Basket') {
+            iconSource = focused
+              ? require('../assets/icons/basket-active.png')
+              : require('../assets/icons/basket.png');
+          } else if (route.name === 'Notification') {
+            iconSource = focused
+              ? require('../assets/icons/notification-active.png')
+              : require('../assets/icons/notification.png');
+          }
+
+          return (
+            <>
+              <Image
+                source={iconSource}
+                style={{width: 24, height: 24}}
+                resizeMode="contain"
+              />
+              <Text>{route.name}</Text>
+            </>
+          );
+        },
+        tabBarLabel: () => null, 
+      })}>
+      <Tab.Screen name="Main" component={Main} />
+      <Tab.Screen name="Love" component={Love} />
+      <Tab.Screen name="Basket" component={Basket} />
+      <Tab.Screen name="Notification" component={Notification} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
