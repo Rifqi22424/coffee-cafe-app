5baea96b0caed38f02bf55811def0323dd3b81b2
rifqi2242 <rifqimuzakki45@gmail.com>
2024-10-20 17:27:25 +0700

    slicing start screen page

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator, NativeStackNavigationProp} from '@react-navigation/native-stack';
 import StartScreen from '../screens/StartScreen';
-// import Main from '../screens/TabScreen/Main';
 import Detail from '../screens/Detail';
 import Order from '../screens/Order';
 import TabNavigator from './TabNavigator';
@@ -25,7 +24,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainTabs" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="StartScreen" screenOptions={{headerShown: false}}>
         <Stack.Screen name="StartScreen" component={StartScreen} />
         <Stack.Screen name="MainTabs" component={TabNavigator} />
         <Stack.Screen name="Detail" component={Detail} />
